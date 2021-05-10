3a32741fefe8633264c3afe49f50bdd4a464bbda
Sofia Mañana <sofiamanana@sansano.usm.cl>
2021-05-10 15:45:08 -0400

    primer commit

---
+++ servidor.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"strings"
+)
+
+func mensaje_bienvenida() {
+	fmt.Printf("Iniciando Servidor\n")
+}
+
+func main() {
+	go mensaje_bienvenida()
+	PUERTO := ":50001"
+	BUFFER := 1024
+	s, err := net.ResolveUDPAddr("udp4", PUERTO)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	connection, err := net.ListenUDP("udp4", s)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer connection.Close()
+	buffer := make([]byte, BUFFER)
+	for {
+		n, addr, err := connection.ReadFromUDP(buffer)
+		fmt.Print("->", string(buffer[0:n-1]))
+
+		if strings.TrimSpace(string(buffer[0:n-1])) == "STOP" {
+			fmt.Println("Exiting UDP Server")
+			return
+		}
+
+		mensaje := []byte("Hola")
+		fmt.Printf("data: %s\n", string(mensaje))
+		_, err = connection.WriteToUDP(mensaje, addr)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+	}
+}
